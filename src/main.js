b1f45c5a3b9a1b4716a8fd94dc70534e227e74ac
Isuru udayanga <isurutmv@gmail.com>
2020-02-23 20:25:12 +0530

    add vuetify js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue';
+import vuetify from './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
+Vue.use(vuetify);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
