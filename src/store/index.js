c7dd03ec1083eaee8479575d003e906e23d78589
Isuru udayanga <isurutmv@gmail.com>
2020-02-24 09:13:29 +0530

    add vuex and fethare vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+// src/store/index.js
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { FeathersVuex } from '../feathers-client'
+import auth from './store.auth'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
+Vue.use(FeathersVuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
-});
+  state: {},
+  mutations: {},
+  actions: {},
+  plugins: [auth]
+})
